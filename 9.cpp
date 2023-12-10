c3af640b492c813fb683c32d88665570368a0f5f
Ayush W <113709655+AshWatts@users.noreply.github.com>
2023-12-10 23:29:14 +0530

    ~

--- 9.cpp
+++ 9.cpp
@@ -1,18 +1,24 @@
-#include <bits/stdc++.h>
-
+#include<bits/stdc++.h>
 using namespace std;
 
-bool isBalanced(string exp) {
-  stack<char> s;
-  for (int i = 0; i < exp.length(); i++) {
-    if (exp[i] == '(' || exp[i] == '[' || exp[i] == '{') {
-      s.push(exp[i]);
-    } else if (exp[i] == ')' || exp[i] == ']' || exp[i] == '}') {
-      if (s.empty()) {
+bool isexp(string e)
+{
+  stack <char> s;
+  for(int i=0;i<e.length();i++)
+  {
+    if(e[i]=='(' || e[i]=='{' || e[i]=='[')
+    {
+      s.push(e[i]);
+    }
+    else if(e[i]==')' || e[i]=='}' || e[i]==']')
+    {
+      if(s.empty())
+      {
         return false;
       }
-      char top = s.top();
-      if ((exp[i] == ')' && top != '(') || (exp[i] == ']' && top != '[') || (exp[i] == '}' && top != '{')) {
+      char top=s.top();
+      if((e[i]==')' && top!='(') || (e[i]=='}' && top!='{') || (e[i]==']' && top!='['))
+      {
         return false;
       }
       s.pop();
@@ -21,14 +27,19 @@ bool isBalanced(string exp) {
   return s.empty();
 }
 
-int main() {
-  string expression;
-  cout << "Enter an expression: ";
-  getline(cin, expression);
-  if (isBalanced(expression)) {
-    cout << "The expression is well parenthesized." << endl;
-  } else {
-    cout << "The expression is not well parenthesized." << endl;
+
+int main()
+{
+  string exp;
+  cout<<"Enter the Expression : ";
+  cin>>exp;
+  if(isexp(exp))
+  {
+    cout<<"Well Parenthesized";
+  }
+  else
+  {
+    cout<<"Not well Parenthesized";
   }
   return 0;
 }
