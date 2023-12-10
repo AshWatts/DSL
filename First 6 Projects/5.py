62b7188386043f8a8a8fbffe04666b43bf704beb
Ayush W <113709655+AshWatts@users.noreply.github.com>
2023-12-10 13:09:36 +0530

    ~

--- First 6 Projects/5.py
+++ First 6 Projects/5.py
@@ -1,10 +1,26 @@
 def bs():
+    for i in range(n):
+        flag = False
+        for j in range(n-i-1):
+            if(l[j]>l[j+1]):
+                (l[j],l[j+1])=(l[j+1],l[j])
+                flag= True
+        if(flag==False):
+            break
+
+    print("Binary Sort : ",l)
+
+def sel_s():
+    for i in range(n):
+        min=i
+        for j in range(i+1,n):
+            if(l[min]>l[j]):
+                min=j
+        l[i],l[min]=l[min],l[i]
+    print("Selection Sort : ",l)
 
 
-#def sel_s():
-
-
-#def is():
+def is():
 
 
 #def shell_s():
@@ -19,7 +35,7 @@ for i in range(n):
     ele = float(input("Enter percentage : "))
     l.append(ele)
 
-#quick_sort(l, 0, n - 1)
+is()
 
 print("Displaying top 5 scores : ")
 for i in range(5):
