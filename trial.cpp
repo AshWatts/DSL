ee9f723f72c6c37eb10cc9f3eaebc547d56a2b10
Ayush W <113709655+AshWatts@users.noreply.github.com>
2024-02-15 23:54:24 +0530

    `

---
+++ trial.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+// use the standard namespace
+using namespace std;
+
+int main() 
+{
+    int a = 10;
+    int b = 20;
+    int sum = a + b;
+    
+// add 19 to sum
+    sum += 19;
+
+    cout << "The sum is: " << sum << std::endl;
+    
+    return 0;
+}
